4fcb0d5969942803d22c4cefb4092abbc8013917
ren <madwolf3189@icloud.com>
2025-03-07 06:19:04 +0000

    ０９課題

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+
+Route::controller(AAAController::class)->group(function() {
+    Route::get('XXX', 'bbb');
+});
+
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
